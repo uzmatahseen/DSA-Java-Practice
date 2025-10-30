b7bb1def9861f023d2b84ec4ddc3ed09761e4bd9
Uzma Tahseen <137533487+uzmatahseen@users.noreply.github.com>
2025-10-30 10:32:33 +0530

    Added ContainsDuplicate.java from LeetCode

---
+++ ContainsDuplicate.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> intSet = new HashSet<>();
+
+        for (int num : nums) {
+            if (intSet.contains(num))
+                return true;
+
+            intSet.add(num);
+        }
+        return false;
+    }
+}
